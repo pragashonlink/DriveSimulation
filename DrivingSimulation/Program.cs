e89e572c6df3f851206c2a6a9e812a1cf0e5c45e
Pragash Rajarathnam <pragash.rajarathnam@thoughtworks.com>
2023-11-20 22:58:51 +0800

    init project structure

---
+++ DrivingSimulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((_, services) =>
+        {
+            
+        });
+}
